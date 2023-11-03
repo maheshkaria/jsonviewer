59e4cc14cedcbd79c150eabbd951e74670773c87
Mahesh Karia <mahesh.karia@here.com>
2023-11-03 23:33:02 +0530

    Added JS

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function() {
+    console.log("booyah!");
+    // $("#json-text-area").val("");
+
+    $("#json-str-btn").addClass("dark:text-teal-700 border-teal-700");
+
+    $("#json-tree-btn").click(function() {
+        $("#json-str-btn").removeClass("dark:text-teal-700 border-teal-700");
+        $("#json-tree-btn").addClass("dark:text-teal-700 border-teal-700");
+        $("#json-text-area").hide();
+
+        if(document.getElementsByClassName("jsontree_label").length === 0) {
+            var wrapper = document.getElementById("wrapper");
+            var tree = jsonTree.create(JSON.parse($("#json-text-area").val()), wrapper);
+        }
+    });
+
+    $("#json-str-btn").click(function() {
+        $("#json-str-btn").addClass("dark:text-teal-700 border-teal-700");
+        $("#json-tree-btn").removeClass("dark:text-teal-700 border-teal-700");
+        $("#json-text-area").show();
+    });
+
+
+
+});
