c836ba7e958e362335884aa571500e5f9cd49f45
Mahesh Karia <mahesh.karia@here.com>
2023-11-05 19:23:29 +0530

    Added empty validation

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ $(function() {
     $("#json-str-btn").addClass("dark:text-teal-700 border-teal-700");
 
     $("#json-tree-btn").click(function() {
-        $("#json-str-btn").removeClass("dark:text-teal-700 border-teal-700");
-        $("#json-tree-btn").addClass("dark:text-teal-700 border-teal-700");
-        $("#json-text-area").hide();
-        $("#wrapper").show();
-
-        if(document.getElementsByClassName("jsontree_label").length === 0) {
-            var wrapper = document.getElementById("wrapper");
-            var tree = jsonTree.create(JSON.parse($("#json-text-area").val()), wrapper);
+        if ($("#json-text-area").val().length > 0) {
+            $("#json-str-btn").removeClass("dark:text-teal-700 border-teal-700");
+            $("#json-tree-btn").addClass("dark:text-teal-700 border-teal-700");
+            $("#json-text-area").hide();
+            $("#wrapper").show();
+            if(document.getElementsByClassName("jsontree_label").length === 0) {
+                var wrapper = document.getElementById("wrapper");
+                var tree = jsonTree.create(JSON.parse($("#json-text-area").val()), wrapper);
+            }
+        } else {
+            alert("JSON can not be empty!");
         }
     });
 
